039fbe0dace34271ef27ccaa9bfa70fe6e28ea55
good4something-6 <mail6mw@gmail.com>
2022-03-28 13:09:54 +0100

    Outline app.test.js file created

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,19 @@ const request = require("supertest");
 const { app } = require("../app");
 const db = require("../db/connection");
 
+
+const seed = require("../db/seeds/seed");
+
+const testData = require('../db/data/test-data/index');
+
+beforeEach(()=>{
+    return seed(testData);
+});
+
+afterAll(()=>{
+    return db.end();
+});
+
 describe("",()=>{
 test("Dummy test",()=>{
     expect("XX").toEqual("XX");
